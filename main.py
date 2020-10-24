787975a7c6209ca942e39bfe4bb36a0a386254fa
Michael List <michael.list.work@gmail.com>
2020-10-24 09:46:41 -0700

    removed todo comments

--- main.py
+++ main.py
@@ -3,19 +3,10 @@ import json
 
 import requests
 
-# Use Like python githubber.py JASchilz
-# (or another user name)
 
 if __name__ == "__main__":
     username = sys.argv[1]
 
-    # TODO:
-    #
-    # 1. Retrieve a list of "events" associated with the given user name
     response = requests.get(f"https://api.github.com/users/{username}/events")
     events = json.loads(response.content)
-    # 2. Print out the time stamp associated with the first event in that list.
     print(events[0]['created_at'])
-    
-
-
